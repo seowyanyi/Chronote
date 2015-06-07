b8e0bec4695180d2ce9387d63168a41aa05d480a
seowyanyi <seow.yanyi@gmail.com>
2015-06-07 15:19:04 +0800

    toggle highlight mode with button

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,36 @@
 'use strict';
 
+var StorageArea = chrome.storage.local;
+
+StorageArea.get("highlight", function(items) {
+    console.log(items);
+    setButtonText(items.highlight);
+});
+
 console.log('\'Allo \'Allo! Popup');
+
+document.getElementById("toggleButton").addEventListener("click",toggleMode);
+
+
+function toggleMode() {
+    StorageArea.get("highlight", function(items) {
+        var newState = !items.highlight;
+
+        setStorage(newState);
+        setButtonText(newState);
+    });
+}
+
+function setStorage(state) {
+    StorageArea.set({'highlight': state});
+}
+
+function setButtonText(state) {
+    var button = document.getElementById("toggleButton");
+    if (state) {
+        button.textContent = "Highlight Mode Off";
+    } else {
+        button.textContent = "Highlight Mode On";
+    }
+}
+
