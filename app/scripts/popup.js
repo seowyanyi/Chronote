6fb2637dbe648e4aebada1b0500236be2fe6bb1d
seowyanyi <seow.yanyi@gmail.com>
2015-06-07 17:13:49 +0800

    refactor

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,20 +1,28 @@
 'use strict';
+//////////////////// CONSTANTS ///////////////////
+var STORAGE_HIGHLIGHT_MODE = "highlightMode";
+/////////////////////////////////////////////////
 
 var StorageArea = chrome.storage.local;
 
-StorageArea.get("highlight", function(items) {
-    console.log(items);
-    setButtonText(items.highlight);
-});
+initButton();
 
-console.log('\'Allo \'Allo! Popup');
-
+console.log('\'Allo \'Allo! Popup access test');
 document.getElementById("toggleButton").addEventListener("click",toggleMode);
 
+function initButton() {
+    StorageArea.get(STORAGE_HIGHLIGHT_MODE, function(items) {
+        console.log(items);
+
+        setButtonText(items.highlightMode);
+    });
+}
 
 function toggleMode() {
-    StorageArea.get("highlight", function(items) {
-        var newState = !items.highlight;
+    StorageArea.get(STORAGE_HIGHLIGHT_MODE, function(items) {
+        console.log(items);
+        console.log(items[STORAGE_HIGHLIGHT_MODE]);
+        var newState = !items[STORAGE_HIGHLIGHT_MODE];
 
         setStorage(newState);
         setButtonText(newState);
@@ -22,15 +30,18 @@ function toggleMode() {
 }
 
 function setStorage(state) {
-    StorageArea.set({'highlight': state});
+    var newState = {};
+    newState[STORAGE_HIGHLIGHT_MODE] = state;
+
+    StorageArea.set(newState);
 }
 
 function setButtonText(state) {
     var button = document.getElementById("toggleButton");
     if (state) {
-        button.textContent = "Highlight Mode Off";
+        button.textContent = "Turn Highlight Mode Off";
     } else {
-        button.textContent = "Highlight Mode On";
+        button.textContent = "Turn Highlight Mode On";
     }
 }
 
